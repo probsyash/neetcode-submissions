dc641bdd2fd7102d4804c7a45f1860a733edf9b3
Yashaskar Karmacharya <myyash.ask@gmail.com>
2026-07-12 18:51:49 +0100

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int min = 0;
+        int max = nums.length - 1;
+        
+        while(min <= max){
+            int mid = min + ((max - min)/2);
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                min = mid + 1;
+            }
+            else if(nums[mid] > target){
+                max = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}

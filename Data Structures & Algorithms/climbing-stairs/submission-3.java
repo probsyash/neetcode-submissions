c0ac4fc02aabe4e32f915496adba1d73102c9a54
Yashaskar Karmacharya <myyash.ask@gmail.com>
2026-07-27 11:07:03 +0100

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] cache;
+    public int climbStairs(int n) {
+        cache = new int[n];
+        for(int i = 0; i < cache.length; i++){
+            cache[i] = -1;
+        }
+        return dfs(n, 0);
+    }
+
+    public int dfs(int n, int i){
+        if(i == n){
+            return 1;
+        }
+        else if(i > n){
+            return 0;
+        }
+
+        if(cache[i] != -1){
+            return cache[i];
+        }
+        else{
+            return cache[i] = dfs(n, i + 1) + dfs(n, i + 2);
+        }
+    }
+}
